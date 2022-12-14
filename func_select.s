324c9f4f1bfee29c62fe8489e965ca242499b9d5
adi-ben-yehuda <adiby2017@gmail.com>
2022-12-14 19:35:36 +0200

    start the func

--- func_select.s
+++ func_select.s
@@ -5,3 +5,46 @@
 .globl run_func
 .type run_func, @function
 run_func:
+        # option in %eax, first pstring in %rsi, second pstring in %rdi
+
+        push    %rbp            # Save the old frame pointer
+        movq    %rsp, %rbp      # for correct debugging
+
+        cmpl    $31, (%eax)       # check if option = 31
+        je      LENGTH
+        cmpl    $32, (%eax)       # check if option = 31
+        je      REPLACE
+        cmpl    $33, (%eax)       # check if option = 31
+        je      REPLACE
+        cmpl    $35, (%eax)       # check if option = 31
+        je      COPY
+        cmpl    $36, (%eax)       # check if option = 31
+        je      SWAP
+        cmpl    $37, (%eax)       # check if option = 31
+        je      COMPARE
+        jmp END
+
+LENGTH:
+        jmp END
+
+REPLACE:
+    jmp END
+
+COPY:
+    jmp END
+
+SWAP:
+    jmp END
+
+COMPARE:
+    jmp END
+
+
+END:
+
+
+
+        movq    %rbp, %rsp
+        popq    %rbp
+        ret
+
--- run_main.s
+++ run_main.s
@@ -27,6 +27,7 @@ run_main:
         movq    $0, %rax        # clear AL (zero FP args in XMM registers)
         movq    $text1, %rsi    # define that the value from scanf will be saved in text1
         call    scanf           # get the first text
+        # TODO: add \0 in the end
 
         movq    $format_scanf_number, %rdi   # load format for scanf for numbers
         movq    $0, %rax        # clear AL (zero FP args in XMM registers)
@@ -37,20 +38,23 @@ run_main:
         movq    $0, %rax        # clear AL (zero FP args in XMM registers)
         movq    $text2, %rsi    # define that the value from scanf will be saved in text2
         call    scanf           # get the second text
+        # TODO: add \0 in the end
 
         movq    $format_scanf_number, %rdi   # load format for scanf for numbers
         movq    $0, %rax        # clear AL (zero FP args in XMM registers)
         movq    $option, %rsi   # define that the value from scanf will be saved in option
         call    scanf           # get the option of the user
 
-        subq    $536, %rsp      # move the %rsp so that there is space to save the four values
+        subq    $528, %rsp      # move the %rsp so that there is space to save the four values
         movq    $n1, -8(%rbp)   # save the first length in the stack
         movq    $text1, -16(%rbp) # save the first text in the stack
         movq    $n2, -280(%rbp)   # save the second length in the stack
         movq    $text2, -288(%rbp) # save the second text in the stack
-        movq    $option, -528(%rbp) # save the second text in the stack
 
-
+        movl	$option, %eax
+        movq    -8(%rbp), %rsi
+        movq    -280(%rbp), %rdi
+        call    run_func
 
         movq    %rbp, %rsp
         popq    %rbp
