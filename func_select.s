f708ebd218dc0846d84fb31fa6c83f943559d1f2
adi-ben-yehuda <adiby2017@gmail.com>
2022-12-18 20:16:45 +0200

    finish replace

--- func_select.s
+++ func_select.s
@@ -6,8 +6,6 @@ format_length:  .string    "first pstring length: %d, second pstring length: %d\
 format_get_char:    .string     "%c"
 format_replace: .string    "old char: %c, new char: %c, first string: %s, second string: %s\n"
 
-
-
 .data
 newChar:    .byte 0
 oldChar:    .byte 0
@@ -18,12 +16,8 @@ trash:      .byte 0
 .type run_func, @function
 run_func:
         # option in %edi, first pstring in %rsi, second pstring in %rdx
-
         push    %rbp            # Save the old frame pointer
         movq    %rsp, %rbp      # for correct debugging
-
-
-     #   push    %rdx
 
         cmpl    $31, %edi     # check if option = 31
         je      LENGTH
@@ -60,47 +54,49 @@ REPLACE:
         movq    %rsi, %r13  # save the first pstring
         movq    %rdx, %r14  # save the second pstring
 
-        movq    $format_get_char, %rdi   # load format for scanf for char
-        movq    $trash, %rsi      # define that the value from scanf will be saved in trash
-        movq    $0, %rax        # clear AL (zero FP args in XMM registers)
-        call    scanf           # get the \n from the previous scanf
+        movq    $format_get_char, %rdi   # load format for scanf for char.
+        movq    $trash, %rsi    # define that the value from scanf will be saved in trash.
+        movq    $0, %rax        # clear AL (zero FP args in XMM registers).
+        call    scanf           # get the \n from the previous scanf.
+
+        movq    $format_get_char, %rdi   # load format for scanf for char.
+        movq    $oldChar, %rsi      # define that the value from scanf will be saved in oldChar.
+        movq    $0, %rax            # clear AL (zero FP args in XMM registers).
+        call    scanf               # get the old char.
 
         movq    $format_get_char, %rdi   # load format for scanf for char
-        movq    $0, %rax            # clear AL (zero FP args in XMM registers)
-        movq    $oldChar, %rsi      # define that the value from scanf will be saved in oldChar
-        call    scanf               # get the old char
-
-        movq    $format_get_char, %rdi   # load format for scanf for char
-        movq    $trash, %rsi      # define that the value from scanf will be saved in trash
+        movq    $trash, %rsi    # define that the value from scanf will be saved in trash
         movq    $0, %rax        # clear AL (zero FP args in XMM registers)
         call    scanf           # get the space from the previous scanf
 
         movq    $format_get_char, %rdi   # load format for scanf for char
-        movq    $0, %rax            # clear AL (zero FP args in XMM registers)
         movq    $newChar, %rsi      # define that the value from scanf will be saved in n1
-        call    scanf               # get the new char. Saved in %rsi
+        movq    $0, %rax            # clear AL (zero FP args in XMM registers)
+        call    scanf               # get the new char.
 
         movq    $0, %rsi
-        movb    (newChar), %sil
+        movb    (newChar), %sil # move the new char to be in smaller register.
         movq    $0, %rdx
-        movb    (oldChar), %dl
+        movb    (oldChar), %dl  # move the old char to be in smaller register.
         movq    %r13, %rdi      # saved the pointer to the first pstring.
         call    replaceChar     # %rax contains the pstring after the switch.
         movq    %rax, %r13      # contains the first pstring after the switch.
 
         movq    $0, %rsi
-        movb    (newChar), %sil
+        movb    (newChar), %sil # move the new char to be in smaller register.
         movq    $0, %rdx
-        movb    (oldChar), %dl
-        movq    %r14, %rdi       # saved the pointer to the first pstring in %rdi.
+        movb    (oldChar), %dl  # move the old char to be in smaller register.
+        movq    %r14, %rdi      # saved the pointer to the second pstring in %rdi.
         call    replaceChar     # %rax contains the pstring after the switch.
-        movq    %rax, %r14       # contains the first pstring after the switch.
+        movq    %rax, %r14      # contains the second pstring after the switch.
 
         movq	$format_replace, %rdi	# load format for printf
-        movq    (oldChar), %rsi      # %rdx contains the old char
-        movq    (newChar), %rdx       # define that the value from scanf will be saved in n1
-        movq    %r13, %rcx
-        movq    %r14, %r8
+        movq    (oldChar), %rsi
+        movq    (newChar), %rdx
+        movq    %r13, %rcx      # save the address of the fisrt pstring
+        addq    $1, %rcx        # because we want the string of the pstring
+        movq    %r14, %r8       # save the address of the second pstring
+        addq    $1, %r8         # because we want the string of the pstring
         movq	$0, %rax
         call	printf
 
@@ -108,7 +104,6 @@ REPLACE:
         popq    %r14    # because it is a callee
 
         jmp END
-
 COPY:
     jmp END
 
